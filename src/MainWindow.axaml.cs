2d5a11ad3210dd675c084091251819848149e716
Ethan Ford <ethan.ford06@gmail.com>
2021-07-15 00:16:17 -0500

    Added clock element, not functional

--- src/MainWindow.axaml.cs
+++ src/MainWindow.axaml.cs
@@ -1,22 +1,39 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System.Reactive.Subjects;
+using System;
+using MirrOS.Models;
 
 namespace MirrOS
 {
     public partial class MainWindow : Window
     {
+        public string currentTime = DateTime.Now.ToString("hh:mm tt");
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowModel() { Time = currentTime };
 #if DEBUG
             this.AttachDevTools();
 #endif
+            
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+        private void UpdateTime()
+        {
+
+            if (currentTime != DateTime.Now.ToString("hh:mm tt"))
+            {
+                currentTime = DateTime.Now.ToString("hh:mm tt");
+
+                var context = this.DataContext as MainWindowModel;
+                context.Time = DateTime.Now.ToString("hh:mm tt");
+            }
         }
     }
 }
---
+++ src/Models/MainWindowModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MirrOS.Models
+{
+    class MainWindowModel : INotifyPropertyChanged
+    {
+
+        private string _time;
+
+        public string Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
